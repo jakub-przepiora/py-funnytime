363c72bc367b8c5f653fb270db32e5f599e64f95
jakub.przepiora <jakub.przepioraa@gmail.com>
2020-07-11 15:37:08 +0200

    file read

--- zabawa.py
+++ zabawa.py
@@ -22,3 +22,25 @@
 #        exit()
 #    i = i + 1
 #print("nie udalo sie")
+
+#==========================#
+#======= ASCII ART ========#
+#==========================#
+
+#import sys
+#from colorama import init
+#init (strip=not sys.stdout.isatty())
+#from termcolor import cprint
+#from pyfiglet import figlet_format
+
+#cprint(figlet_format('missile!', font='starwars' ), 'yellow', 'on_red', attrs=['bold'])
+
+print("Otwieranie pliku ...")
+#afile = open("plik.txt",'r')
+
+with open("plik.txt", 'r') as afile:
+    #afile.read()
+    print(afile.read(30))
+    
+
+afile.close()
