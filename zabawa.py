b7f43cd8180cd17a7f16df9aec2cfb89f737c720
jakub-przepiora <jakub.przepioraa@gmail.com>
2020-07-11 13:46:53 +0200

    start fun

---
+++ zabawa.py
@@ -0,0 +1,24 @@
+#===========================#
+#======= First program =====#
+#===========================#
+
+#from random import seed
+#from random import random
+#from random import randint
+
+#print( random() )
+
+
+#value = randint(0, 10)
+#print(value)
+#print("Liczba zostala wylosowana dla Ciebie, sprobuj trafic: ")
+
+
+#i = 0
+#while i != 4:
+#    odp = input()
+#    if value == int(odp):
+#        print( "niece" )
+#        exit()
+#    i = i + 1
+#print("nie udalo sie")
